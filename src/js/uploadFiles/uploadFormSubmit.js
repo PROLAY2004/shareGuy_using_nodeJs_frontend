15473573d3df5d158c1b8b5eaa5e7f5459d44673
Prolay <prolayhalder2004@gmail.com>
2025-11-23 00:55:36 +0530

    Feat : File Uploading from frontend

--- src/js/uploadFiles/uploadFormSubmit.js
+++ src/js/uploadFiles/uploadFormSubmit.js
@@ -1,29 +1,41 @@
+import configaration from "../config/config.js";
+
 const filesInput = document.getElementById("selectFiles");
 const fileUploadform = document.getElementById("fileUploadform");
 const shareSection = document.getElementById("shareSection");
+const spinner = document.getElementById("spinner");
 
-export function uploadFiles(event) {
-  event.preventDefault();
-
-  shareSection.classList.remove("d-none");
-  fileUploadform.classList.add("d-none");
-
-  // show Alert if someone want to reload page while a ongoing session
-  window.addEventListener("beforeunload", function (event) {
+export async function uploadFiles(event) {
+  try {
     event.preventDefault();
-    event.returnValue = "";
-  });
 
-  const formData = new FormData();
+    // show Alert if someone want to reload page while a ongoing session
+    window.addEventListener("beforeunload", function (event) {
+      event.preventDefault();
+      event.returnValue = "";
+    });
 
-  console.log("submitting...");
+    const formData = new FormData();
 
-  for (let i = 0; i < filesInput.files.length; i++) {
-    console.log(i);
-    formData.append("uploadedFile[]", filesInput.files[i]);
-  }
+    for (let i = 0; i < filesInput.files.length; i++) {
+      formData.append("selectFiles[]", filesInput.files[i]);
+    }
 
-  for (const pair of formData.entries()) {
-    console.log(pair[0], pair[1]);
+    spinner.classList.remove("d-none");
+
+    const res = await fetch(`${configaration.BASE_URL}/upload`, {
+      method: "POST",
+      headers: {},
+      body: formData,
+    });
+
+    spinner.classList.add("d-none");
+    shareSection.classList.remove("d-none");
+    fileUploadform.classList.add("d-none");
+
+    // const data = await res.json();
+    console.log(res);
+  } catch (err) {
+    console.log(err.message);
   }
 }
