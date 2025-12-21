0773fca3e5edf707192305115d4777edf8fb9500
Prolay <prolayhalder2004@gmail.com>
2025-12-21 22:46:27 +0530

    Feat : socket toast added

--- src/js/socket/initSocket.js
+++ src/js/socket/initSocket.js
@@ -1,11 +1,20 @@
 import { io } from 'socket.io-client';
+import * as bootstrap from 'bootstrap';
 
 import configaration from '../config/config.js';
 import { downloadFile } from '../downloadFiles/fileDownload.js';
+import ToastTemplates from '../templates/ToastTemplates.js';
 
 export default async function initSocket() {
+	const scannerModal = document.getElementById('scannerModal');
 	const socket = io(configaration.BASE_URL);
 	const sendFileBtn = document.getElementById('sendFileBtn');
+	const toastSection = document.getElementById('toastSection');
+
+	const displayToast = new ToastTemplates();
+	const openScannerModal =
+		bootstrap.Modal.getInstance(scannerModal) ||
+		new bootstrap.Modal(scannerModal);
 
 	socket.on('connect', () => {
 		localStorage.setItem('socketId', socket.id);
@@ -15,6 +24,17 @@ export default async function initSocket() {
 			const shareCodeId = sendFileBtn.getAttribute('data-shareCode');
 
 			socket.emit('send-file', { roomId, shareCodeId });
+
+			sendFileBtn.setAttribute('Disabled', '');
+			openScannerModal.hide();
+
+			toastSection.innerHTML = displayToast.successToast(
+				'File Sent Successfully'
+			);
+
+			setTimeout(() => {
+				toastSection.innerHTML = '';
+			}, 3000);
 		});
 
 		socket.on('receive-file', (shareCodeId) => {
