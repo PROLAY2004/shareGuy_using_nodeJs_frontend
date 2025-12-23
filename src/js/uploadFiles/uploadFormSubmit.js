e2addc6ed1a0db78d4ce4175cc6ae940b1d2239e
Prolay <prolayhalder2004@gmail.com>
2025-12-23 21:35:08 +0530

    fix : user upload fixed

--- src/js/uploadFiles/uploadFormSubmit.js
+++ src/js/uploadFiles/uploadFormSubmit.js
@@ -1,54 +1,67 @@
-import configaration from "../config/config.js";
-import endTransfer from "./endTransfer.js";
-import ToastTemplates from "../templates/ToastTemplates.js";
+import QRCode from 'qrcode';
+
+import configaration from '../config/config.js';
+import endTransfer from './endTransfer.js';
+import ToastTemplates from '../templates/ToastTemplates.js';
 
 const displayToast = new ToastTemplates();
-const toastSection = document.getElementById("toastSection");
-const filesInput = document.getElementById("selectFiles");
-const fileUploadform = document.getElementById("fileUploadform");
-const shareSection = document.getElementById("shareSection");
-const spinner = document.getElementById("spinner");
-const qrImage = document.getElementById("qrImage");
-const uniCodeArea = document.getElementById("uniCodeArea");
-const endTransferBtn = document.getElementById("endTransferBtn");
+const toastSection = document.getElementById('toastSection');
+const filesInput = document.getElementById('selectFiles');
+const fileUploadform = document.getElementById('fileUploadform');
+const shareSection = document.getElementById('shareSection');
+const spinner = document.getElementById('spinner');
+const qrImage = document.getElementById('qrImage');
+const uniCodeArea = document.getElementById('uniCodeArea');
+const endTransferBtn = document.getElementById('endTransferBtn');
+
+let beforeUnloadAttached = false;
 
 export async function uploadFiles(event) {
-  try {
-    event.preventDefault();
+	try {
+		event.preventDefault();
 
-    // show Alert if someone want to reload page while a ongoing session
-    window.addEventListener("beforeunload", endTransfer);
+		if (!beforeUnloadAttached) {
+			// show Alert if someone want to reload page while a ongoing session
+			window.addEventListener('beforeunload', endTransfer);
+			beforeUnloadAttached = true;
+		}
 
-    const formData = new FormData();
+		const formData = new FormData();
 
-    for (let i = 0; i < filesInput.files.length; i++) {
-      formData.append("selectFiles[]", filesInput.files[i]);
-    }
+		for (let i = 0; i < filesInput.files.length; i++) {
+			formData.append('selectFiles[]', filesInput.files[i]);
+		}
 
-    spinner.classList.remove("d-none");
+		spinner.classList.remove('d-none');
 
-    const res = await fetch(`${configaration.BASE_URL}/upload`, {
-      method: "POST",
-      headers: {},
-      body: formData,
-    });
+		const res = await fetch(`${configaration.BASE_URL}/upload`, {
+			method: 'POST',
+			body: formData,
+		});
 
-    const data = await res.json();
+		const data = await res.json();
 
-    spinner.classList.add("d-none");
-    shareSection.classList.remove("d-none");
-    fileUploadform.classList.add("d-none");
+		if (data.success) {
+			spinner.classList.add('d-none');
+			shareSection.classList.remove('d-none');
+			fileUploadform.classList.add('d-none');
 
-    qrImage.setAttribute("src", data.session.qrPath);
-    uniCodeArea.innerHTML = data.session.code;
+			qrImage.src = await QRCode.toDataURL(
+				`${configaration.BASE_URL}/download/${data.data.code}`
+			);
+			uniCodeArea.innerHTML = data.data.code;
 
-    endTransferBtn.setAttribute("data-code", data.session.code);
-  } catch (err) {
-    spinner.classList.add('d-none');
-    toastSection.innerHTML = displayToast.errorToast(err.message);
-
-    setTimeout(() => {
-      toastSection.innerHTML = "";
-    }, 3000);
-  }
+			endTransferBtn.setAttribute('data-code', data.data.code);
+		} else {
+			spinner.classList.add('d-none');
+			toastSection.innerHTML = displayToast.errorToast(data.message);
+		}
+	} catch (err) {
+		spinner.classList.add('d-none');
+		toastSection.innerHTML = displayToast.errorToast(err.message);
+	} finally {
+		setTimeout(() => {
+			toastSection.innerHTML = '';
+		}, 3000);
+	}
 }
