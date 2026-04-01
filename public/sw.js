4306b9b32564038f83c33d6b6f24c5c0f6618c6c
Prolay Halder <130995686+PROLAY2004@users.noreply.github.com>
2026-04-01 13:11:37 +0530

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10814242
+    "domain": "5gvci.com",
+    "zoneId": 10814248
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
